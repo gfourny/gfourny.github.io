82b44cdc16ff078aa8d1f7643c93665f85cf9a18
gfourny <fourny.g@free.fr>
2018-11-08 13:37:59 +0100

    reinitialisation du projet

---
+++ precache-manifest.6394356cd22350c3275e8ae003d1d84d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3fdf7228dfa9306d24c7",
+    "url": "/js/chunk-vendors.9dc9a68c.js"
+  },
+  {
+    "revision": "a6333aad9172828a3488",
+    "url": "/js/app.3b6587ab.js"
+  },
+  {
+    "revision": "74bd2a379465451c97f0",
+    "url": "/js/about.1bdda452.js"
+  },
+  {
+    "revision": "5bb01bcad39863247b1e9dbfba6b5cbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a6333aad9172828a3488",
+    "url": "/css/app.d7c37f8e.css"
+  }
+];
