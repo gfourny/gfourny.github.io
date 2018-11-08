ee169cf23d6b9460284e5dc8d7f71fdb3be6dd77
gfourny <fourny.g@free.fr>
2018-11-08 15:27:22 +0100

    first commit

---
+++ precache-manifest.4189a7520897afa2b6e56c632988e7ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e7bc974551e520f5aca7",
+    "url": "/js/chunk-vendors.df2dc307.js"
+  },
+  {
+    "revision": "77df89953fa43e1e1d36",
+    "url": "/js/app.d94e1e49.js"
+  },
+  {
+    "revision": "74bd2a379465451c97f0",
+    "url": "/js/about.1bdda452.js"
+  },
+  {
+    "revision": "5cd965dcf5518e4ca1b279c393f7470b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e7bc974551e520f5aca7",
+    "url": "/css/chunk-vendors.7b2b93f7.css"
+  },
+  {
+    "revision": "77df89953fa43e1e1d36",
+    "url": "/css/app.a6364da1.css"
+  }
+];
